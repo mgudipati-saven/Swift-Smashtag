eb7f174e549a1bbc157052467d8bb09db7d7f385
Murty Gudipati <murty.gudipati@saven.in>
2015-03-08 22:00:25 +0530

    TableView

---
+++ Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by Murty Gudipati on 3/8/15.
+//  Copyright (c) 2015 Murty Gudipati. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell
+{
+    var tweet: Tweet? {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    @IBOutlet weak var tweetProfileImageView: UIImageView!
+    @IBOutlet weak var tweetScreenNameLabel: UILabel!
+    @IBOutlet weak var tweetTextLabel: UILabel!
+    
+    func updateUI() {
+        // reset any existing tweet information
+        tweetTextLabel?.attributedText = nil
+        tweetScreenNameLabel?.text = nil
+        tweetProfileImageView?.image = nil
+        
+        // load new information from our tweet (if any)
+        if let tweet = self.tweet {
+            tweetTextLabel?.text = tweet.text
+            if tweetTextLabel?.text != nil {
+                for _ in tweet.media {
+                    tweetTextLabel.text! += " "
+                }
+            }
+            
+            tweetScreenNameLabel?.text = "\(tweet.user)"
+            
+            if let profileImageURL = tweet.user.profileImageURL {
+                if let imageData = NSData(contentsOfURL: profileImageURL) {
+                    tweetProfileImageView?.image =  UIImage(data: imageData)
+                }
+            }
+        }
+    }
+}
